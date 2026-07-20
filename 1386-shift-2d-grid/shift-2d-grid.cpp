82c5425dafcd4f3edda4c4fde7575c76149d0259
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-07-20 20:14:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (39.39%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int total = m * n;
+        k %= total;
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                int r = newIndex / n;
+                int c = newIndex % n;
+
+                ans[r][c] = grid[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
