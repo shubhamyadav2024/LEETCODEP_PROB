34b7030d67c99f19c40afcf3df570d4e84d1f8de
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-07-21 08:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (66.11%) - LeetSync

---
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int mn = nums[0];
+        int ans = -1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > mn)
+                ans = max(ans, nums[i] - mn);
+
+            mn = min(mn, nums[i]);
+        }
+
+        return ans;
+    }
+};
