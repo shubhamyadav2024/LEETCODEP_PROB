84f1a33ed6eda997a1608c8c832ebb0bff35f7ba
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-07-21 08:20:30 +0530

    Time: 138 ms (17.11%) | Memory: 175.9 MB (11.71%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        string t = "1" + s + "1";
+
+        vector<pair<char,int>> runs;
+        for(char c : t) {
+            if(runs.empty() || runs.back().first != c)
+                runs.push_back({c,1});
+            else
+                runs.back().second++;
+        }
+
+        int ones = 0;
+        for(char c : s)
+            if(c == '1') ones++;
+
+        int ans = ones;
+
+        for(int i = 1; i + 1 < runs.size(); i++) {
+            if(runs[i-1].first == '0' &&
+               runs[i].first == '1' &&
+               runs[i+1].first == '0') {
+
+                ans = max(ans,
+                          ones + runs[i-1].second + runs[i+1].second);
+            }
+        }
+
+        return ans;
+    }
+};
