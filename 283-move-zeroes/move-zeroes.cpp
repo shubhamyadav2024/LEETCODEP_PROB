50a260180fcaf7429c8df17352094ef897dfbb41
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-08-15 18:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int last=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[last],nums[i]);
+                last++;
+            }
+        }
+    }
+};
