c789e07d433d6737ef1df4fa3860723921833572
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-09-08 23:31:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     int countCommas(int n) {
         
-       long long ans=0;
-       for(int i=1;i<=n;i++){
-        if(i>=1000) ans++;
-        if(i>=1000000) ans++;
-        if(i>=1000000000) ans++;
-        
-       }
-       return ans;
+      if(n<1000) return 0;
+      return (n-1000)+1;
     }
 };
