daaa7bd8cd1698e894e0a244acea62a4b4430bcc
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-09-09 17:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (94.77%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
+
+        reverse(s.begin(), s.end());
+
+        for (int i = 0; i < n; i++) {
+
+            // skip spaces
+            while (i < n && s[i] == ' ') {
+                i++;
+            }
+
+            string word = "";
+
+            // collect word
+            while (i < n && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+
+            reverse(word.begin(), word.end());
+
+            if (word.length() > 0) {
+                if (ans.length() > 0)
+                    ans += " ";
+
+                ans += word;
+            }
+        }
+
+        return ans;
+    }
+};
