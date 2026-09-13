9141528df0a736625e57e329098e67282f0178e7
shubhamyadav2024 <shubham.yadav2024@glbajajgroup.org>
2026-09-13 15:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (56.26%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+      if(s.length() != goal.length()) return false;
+    
+      for(int i=0;i<n;i++){
+        rotate(s.begin(),s.begin()+1,s.end());
+        if(s==goal) return true;
+      }  
+      return false;
+    }
+};
